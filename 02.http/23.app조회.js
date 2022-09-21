d15ab1bb175c766b7ee4bcb54a3d8c0adb4a2bfd
Hayeongjin <eclips1024@naver.com>
2022-09-21 14:37:01 +0900

    node 3일차 공부

---
+++ 02.http/23.app조회.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const view = require('./view/index');
+const template = require('./view/template');
+
+http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    let query = url.parse(req.url, true).query;
+    switch (pathname) {
+        case '/':
+            if (query.id === undefined) { //초기화면
+                fs.readdir('data', (err, files) => {
+                    const title = '웹 기술';
+                    const list = template.listGen(files);
+                    const contents = template.HOMT_CONTENTS;
+                    const html = view.index(title, list, contents);
+                    res.end(html);
+                });
+
+            } else {                      // 개별 아이템에 대한 화면
+                fs.readdir('data', (err, files) => {
+                    const title = query.id;
+                    const list = template.listGen(files);
+                    const filename = `data/${query.id}.txt`;
+                    fs.readFile(filename, 'utf-8', (err, contents) => {
+                        const html = view.index(title, list, contents);
+                        res.end(html);
+                    });
+                });
+            }
+
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end();
+    }
+}).listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
--- 02.http/view/index.js
+++ 02.http/view/index.js
@@ -1,4 +1,4 @@
-exports.index = function(){
+exports.index = function(title,list,contents){
     return `
     <!DOCTYPE html>
 <html lang="ko">
@@ -6,23 +6,16 @@ exports.index = function(){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>웹 기술</title>
+    <title>${title}</title>
 </head>
 <body style="margin:50px">
-    <h1>웹 기술</h1>
+    <h1><a href="/">웹 기술</a></h1>
     <ul>
-        <li><h3>CSS</h3></li>
-        <li><h3>HTML</h3></li>
-        <li><h3>JavaScript</h3></li>
+      ${list} <!--22.appList.js에서 변경된 내용 -->
     </ul>
     <hr>
     <p>
-        월드 와이드 웹(World Wide Web, WWW, W3)은 인터넷에 연결된 컴퓨터를 통해 사람들이 정보를 공유할 수 있는 전 세계적인 정보 공간을 말한다. 
-        간단히 웹(the Web)이라 부르는 경우가 많다. 
-        이 용어는 인터넷과 동의어로 쓰이는 경우가 많으나 엄격히 말해 서로 다른 개념이다.
-        웹은 전자 메일과 같이 인터넷 상에서 동작하는 하나의 서비스일 뿐이다. 
-        그러나 1993년 이래로 웹은 인터넷 구조의 절대적 위치를 차지하고 있다.
-        
+        ${contents} <!--app 조회에서 변경된 내용 -->
     </p>   
 </body>
 </html>
