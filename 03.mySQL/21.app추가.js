d62a62dc664035916a99ca3d713435626336d7c4
Hayeongjin <eclips1024@naver.com>
2022-09-26 17:59:41 +0900

    sql http server

---
+++ 03.mySQL/21.app추가.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const url = require('url');
+const mysql = require('mysql');
+const config = require('./mysql.json');
+const template = require('./view/template');
+const qs = require('querystring');
+
+http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    let query = url.parse(req.url, true).query;
+    switch (pathname) {
+        case '/':
+            const conn = mysql.createConnection(config);
+            conn.connect();
+            const sql = `SELECT * FROM tigers WHERE isDeleted=0;`;
+            conn.query(sql, (err, rows, fields) => {
+                if (err) {
+                    throw err;
+                }
+                let trs = template.trsGen(rows);
+                let html = template.home(trs);
+                res.end(html);
+            });
+            conn.end();
+            break;
+        case '/create':
+            if (req.method === 'GET') {
+                let html = template.createForm();
+                res.end(html);
+            } else {
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                req.on('end', () => {
+                    const param = qs.parse(body);
+                    let player = param.player;
+                    let backNo = parseInt(param.backNo);
+                    let position = param.position;
+                    const conn = mysql.createConnection(config);
+                    conn.connect();
+                    let sql = `INSERT INTO tigers (player,backNo,POSITION)
+                    VALUES (?,?,?);`;
+                    conn.query(sql,[player,backNo,position],(err,fields)=>{
+                        if(err)
+                            throw err;
+                        res.writeHead(302,{'Location':'/'});
+                        res.end()
+                    });
+                    conn.end();
+                });
+            }
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end();
+    }
+}).listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
