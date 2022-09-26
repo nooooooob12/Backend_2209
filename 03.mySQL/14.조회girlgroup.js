c9ef6c8b39c7f9dde0c8d0c04deadffa90d81634
Hayeongjin <eclips1024@naver.com>
2022-09-26 12:44:25 +0900

    sql

---
+++ 03.mySQL/14.조회girlgroup.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const config = require('./mysql.json');
+const conn = mysql.createConnection(config);
+
+conn.connect();
+
+const sql = `SELECT gid, name, DATE_FORMAT(debut, '%Y-%m-%d')AS debutDate, song.title FROM girl_group 
+JOIN song ON girl_group.hit_song_id = song.sid
+`;
+conn.query(sql,(err,rows,fields) =>{
+    if(err){
+        throw err;
+    }
+    for(let row of rows){
+         console.log(`${row.gid}\t${row.name}\t${row.debutDate}\t${row.title}`);
+    } 
+});
+conn.end();
